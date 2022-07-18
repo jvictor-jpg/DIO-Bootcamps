7fd0be5e0ec900f1651e0f52e011eed1e691063f
Victor Silva <65427348+jvictor-jpg@users.noreply.github.com>
2022-07-17 21:33:01 -0300

    Add files via upload

---
+++ 3.React-JS/github-viwer/src/components/no-search/styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+    margin: 32px;
+    display: flex;
+    justify-content: center;
+    font-weight: bold;
+    font-size: 32px;
+  
+`;
---
+++ 3.React-JS/github-viwer/src/components/profile/index.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import useGithub from '../../hooks/github-hooks';
+import * as S from './styled';
+
+function Profile() {
+
+    const {githubState} = useGithub();
+
+    return (
+        <S.Wrapper>
+            <S.WrapperImage src={githubState.user.avatar} alt="User Avatar"/>
+            <S.UserInfo>
+                <div>
+                    <h1>{githubState.user.name}</h1>
+                    <S.WrapperUserInfo>
+                        <h3>Username: </h3>
+                        <a href={githubState.user.html_url} target='_blank' rel='noreferrer'>
+                            {githubState.user.login}
+                        </a>
+                    </S.WrapperUserInfo>
+                    <S.WrapperUserInfo>
+                        <h3>Company: </h3>
+                        <span>
+                            {githubState.user.company}
+                        </span>
+                    </S.WrapperUserInfo>
+                    <S.WrapperUserInfo>
+                        <h3>Location: </h3>
+                        <span>
+                            {githubState.user.location}
+                        </span>
+                    </S.WrapperUserInfo>
+                    <S.WrapperUserInfo>
+                        <h3>Blog: </h3>
+                        <a href={githubState.user.blog} target='_blank' rel='noreferrer'>
+                            {githubState.user.blog}
+                        </a>
+                    </S.WrapperUserInfo>
+                </div>
+                <S.Status>
+                <div>
+                    <h4>Followers</h4>
+                    <span>{githubState.user.followers}</span>
+                </div>
+                <div>
+                    <h4>Following</h4>
+                    <span>{githubState.user.following}</span>
+                </div>
+                <div>
+                    <h4>Gists</h4>
+                    <span>{githubState.user.public_gists}</span>
+                </div>
+                <div>
+                    <h4>Repos</h4>
+                    <span>{githubState.user.public_repos}</span>
+                </div>
+                </S.Status>
+            </S.UserInfo>
+        </S.Wrapper>
+    )
+}
+
+export default Profile
